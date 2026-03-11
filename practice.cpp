37571782a34d00d62fa9c284ce3b0df0187610eb
joohyeong <ji621638@gmail.com>
2026-03-11 17:41:20 +0900

    hi

---
+++ practice.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	/* {
+		const std::string s = "a string";
+		std::cout << s << std::endl;
+		{
+			const std::string s = "another string";
+			std::cout << s << std::endl;
+		}}
+		*/
+/* {
+		std::string s = "a string";
+		{
+			std::string x = s + ", really";
+			std::cout << x << std::endl;
+		}
+
+	}*/
+
+	/*
+	std::cout << "What is your name? ";
+	std::string name;
+	std::cin >> name;
+	std::cout << "Hello, " << name << std::endl << "And what is yours? ";
+	std::cin >> name;
+	std::cout << "Hello, " << name << "; nice to meet you too!" << std::endl;
+	*/
+	return 0;
+}
