e361aeb9a33593658f37014eae0d03e9549a6a9e
Joohyung-ux <ji621638@gmail.com>
2026-03-19 11:36:27 +0900

    codes2

---
+++ 2-7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main(void) {
+	int i = 10;
+  while (i >= -5) {
+		std::cout << i << std::endl;
+		i -= 1;
+		
+	}
+	std::cout << "finished!" << std::endl;
+	return 0;
+}
